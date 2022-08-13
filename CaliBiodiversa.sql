985a3140999cfa11f31f18004299c90c834746e6
Geovanny <cali.arborea@gmail.com>
2022-08-13 13:36:19 -0500

    GBIF fauna records spatial segmentation acording to Cali territorial information

--- CaliBiodiversa.sql
+++ CaliBiodiversa.sql
@@ -74,5 +74,70 @@ SELECT nombre_cie, COUNT(DISTINCT id_arbol) as arboles FROM censo_arboreo GROUP 
 SELECT nombre_cie AS nombre_tecnico, nombre_com AS nombre_comun, COUNT(DISTINCT id_arbol) AS num_ejemplares FROM censo_arboreo GROUP BY nombre_cie,
 nombre_com ORDER BY num_ejemplares DESC;
 
+/* Spatial linkage and integration of territorial information of the District of Cali in the GBIF databases of fauna species records
+(Mammals, reptiles, anphibians and insects presence records)*/
+
+ALTER TABLE fauna
+ADD COLUMN land_use varchar(10) default 'No aplica',
+ADD COLUMN comuna varchar(10) default 'No aplica',
+ADD COLUMN barrio varchar(70) default 'No aplica',
+ADD COLUMN corregimiento varchar(70) default 'No aplica';
+
+-- Spatial segmentation of the pollinators PostGIS layer (Families: Apidae, Halictidae, Colletidae and Megachilidae presence records)
+
+ALTER TABLE polinizadores
+ADD COLUMN land_use varchar(10) default 'No aplica',
+ADD COLUMN comuna varchar(10) default 'No aplica',
+ADD COLUMN barrio varchar(70) default 'No aplica',
+ADD COLUMN corregimiento varchar(70) default 'No aplica';
+
+-- Spatial segmentation of the GBIF Plantae data for the Cali's administrative territory
+
+ALTER TABLE gbif_plantae_cali
+ADD COLUMN land_use varchar(10) default 'No aplica',
+ADD COLUMN comuna varchar(10) default 'No aplica',
+ADD COLUMN barrio varchar(70) default 'No aplica',
+ADD COLUMN corregimiento varchar(70) default 'No aplica';
+
+-- The process for the GBIF birds PostGIS layer is omitted because of the size of the layer...
+
+-- land use segmentation
+
+UPDATE fauna SET land_use = 'Urbano' FROM perimetro_urbano WHERE ST_INTERSECTS(fauna.geom, perimetro_urbano.geom);
+UPDATE fauna SET land_use = 'Rural' FROM corregimientos WHERE ST_INTERSECTS(fauna.geom, corregimientos.geom);
+UPDATE fauna SET land_use = 'Expansion' FROM perimetro_expansion WHERE ST_INTERSECTS(fauna.geom, perimetro_expansion.geom);
+
+UPDATE gbif_plantae_cali SET land_use = 'Urbano' FROM perimetro_urbano WHERE ST_INTERSECTS(gbif_plantae_cali.geom, perimetro_urbano.geom);
+UPDATE gbif_plantae_cali SET land_use = 'Rural' FROM corregimientos WHERE ST_INTERSECTS(gbif_plantae_cali.geom, corregimientos.geom);
+UPDATE gbif_plantae_cali SET land_use = 'Expansion' FROM perimetro_expansion WHERE ST_INTERSECTS(gbif_plantae_cali.geom, perimetro_expansion.geom);
+
+UPDATE polinizadores SET land_use = 'Urbano' FROM perimetro_urbano WHERE ST_INTERSECTS(polinizadores.geom, perimetro_urbano.geom);
+UPDATE polinizadores SET land_use = 'Rural' FROM corregimientos WHERE ST_INTERSECTS(polinizadores.geom, corregimientos.geom);
+UPDATE polinizadores SET land_use = 'Expansion' FROM perimetro_expansion WHERE ST_INTERSECTS(polinizadores.geom, perimetro_expansion.geom);
+
+-- Comunas, neighborhoods and corregimientos segmentation
+
+UPDATE fauna SET comuna = comunas.nombre FROM comunas WHERE ST_INTERSECTS(fauna.geom, comunas.geom);
+UPDATE gbif_plantae_cali SET comuna = comunas.nombre FROM comunas WHERE ST_INTERSECTS(gbif_plantae_cali.geom, comunas.geom);
+UPDATE polinizadores SET comuna= comunas.nombre FROM comunas WHERE ST_INTERSECTS(polinizadores.geom, comunas.geom);
+
+UPDATE fauna SET barrio = barrios.barrio FROM barrios WHERE ST_INTERSECTS(fauna.geom, barrios.geom);
+UPDATE gbif_plantae_cali SET barrio = barrios.barrio FROM barrios WHERE ST_INTERSECTS(gbif_plantae_cali.geom, barrios.geom);
+UPDATE polinizadores SET barrio = barrios.barrio FROM barrios WHERE ST_INTERSECTS(polinizadores.geom, barrios.geom);
+
+UPDATE fauna SET corregimiento = corregimientos.corregimie FROM corregimientos WHERE ST_INTERSECTS(fauna.geom, corregimientos.geom);
+UPDATE gbif_plantae_cali SET corregimiento = corregimientos.corregimie FROM corregimientos WHERE ST_INTERSECTS(gbif_plantae_cali.geom, corregimientos.geom);
+UPDATE polinizadores SET corregimiento = corregimientos.corregimie FROM corregimientos WHERE ST_INTERSECTS(polinizadores.geom, corregimientos.geom);
+
+
+
+
+
+
+
+
+
+
+
 
 
